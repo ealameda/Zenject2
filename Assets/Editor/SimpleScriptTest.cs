29b7a81c5de08474149aac8ec0d0e24bd6096910
syntactical <greg.dutcher@gmail.com>
2016-10-05 15:11:58 -0400

    tests working with code

---
+++ Assets/Editor/SimpleScriptTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using NSubstitute;
+
+public class NewEditorTest {
+
+	[Test]
+	public void EditorTest()
+	{
+	    var simpleDependencyMock = Substitute.For<ISimpleDependency>();
+		
+		var simpleScript = new SimpleScript(simpleDependencyMock);
+
+        simpleDependencyMock.Received().SayHello();
+	}
+}
